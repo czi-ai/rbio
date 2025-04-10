ed2e29dd3bbd20844e71bcddf281a8672d7757bf
Fausto Milletari <fmilletari@chanzuckerberg.com>
2025-04-09 17:57:44 -0700

    added gitignore and added regex based rewards to code

--- utils/rewards.py
+++ utils/rewards.py
@@ -1,4 +1,6 @@
 from utils.verifiers import check_math_solution, test_code_solution, test_vcm_task
+import re
+
 
 def math_reward_func(prompts, completions, task, **kwargs):
     """
@@ -21,6 +23,7 @@ def math_reward_func(prompts, completions, task, **kwargs):
             # Return None for non-math tasks
             rewards.append(None)
     return rewards
+
 
 def coding_reward_func(prompts, completions, task, **kwargs):
     """
@@ -78,4 +81,54 @@ def reward_len(completions, **kwargs):
         rewards: list of rewards accumulated by checking the prompts
     """
     rewards = [-abs(20 - len(completion)) for completion in completions]
+    return rewards
+
+
+def is_tag_usage_valid(text: str, tag: str):
+    pattern = fr'</?{tag}>'
+    tags = list(re.finditer(pattern, text))
+    stack = []
+    for t in tags:
+        if t.group() == f'<{tag}>':
+            if stack:
+                return 0  # nested tag
+            stack.append(t.start())
+        else:  # closing tag
+            if not stack:
+                return 0  # unmatched closing tag
+            stack.pop()
+    return 1 if not stack else 0
+
+
+def has_tag(text: str, tag: str):
+    pattern = fr'<{tag}>.*?</{tag}>'
+    return 1 if re.search(pattern, text, re.DOTALL) else 0
+
+
+def formatting_reward(completions, **kwargs):
+    rewards = []
+    for completion in completions:
+        reward = (is_tag_usage_valid(completion, 'think') +
+                  is_tag_usage_valid(completion, 'answer') +
+                  has_tag(completion, 'think') +
+                  has_tag(completion, 'answer')
+                  ) / 4.0
+
+        rewards.append(reward)
+
+    return rewards
+
+
+def is_answer_yes(text):
+    match = re.search(r'<answer>\s*(yes|no)\s*</answer>', text, re.IGNORECASE)
+    if match:
+        return match.group(1).strip().lower() == 'yes'
+    return False
+
+
+def correctness_reward(completions, labels):
+    rewards = []
+    for completion, label in zip(completions, labels):
+        rewards.append(float(is_answer_yes(completion) == bool(label)))
+
     return rewards
