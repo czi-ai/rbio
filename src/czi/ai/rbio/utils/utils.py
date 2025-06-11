11796ddc07993a77b719d7800a4e5873b5e24592
Ana-Maria Istrate <aistrate@chanzuckerberg.com>
2025-06-11 01:46:39 +0000

    Add verifier for cell-type capability

--- src/czi/ai/rbio/utils/utils.py
+++ src/czi/ai/rbio/utils/utils.py
@@ -1,14 +1,18 @@
 import re
 
 
-def extract_answer(text):
-    found = re.search(r"<answer>\s*(yes|no)\s*</answer>", text, re.IGNORECASE)
-    if found:
-        if found.group(1).strip().lower() == "yes":
-            return True
-        if found.group(1).strip().lower() == "no":
-            return False
-
+def extract_answer(text, binary = False):
+    if binary:
+        found = re.search(r"<answer>\s*(yes|no)\s*</answer>", text, re.IGNORECASE)
+        if found:
+            if found.group(1).strip().lower() == "yes":
+                return True
+            if found.group(1).strip().lower() == "no":
+                return False
+    else:
+        found = re.search(r"<answer>(.*?)</answer>", text, re.IGNORECASE)
+        if found:
+            return found.group(1).strip().lower()
     return None
 
 
