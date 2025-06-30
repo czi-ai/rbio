fdd8c8b69a21bd1211d1e08f2cc8a5052d755e8e
Ana-Maria Istrate <aistrate@chanzuckerberg.com>
2025-06-30 05:51:12 +0000

    feat: add functionality for GO-based rewards

--- src/czi/ai/rbio/utils/utils.py
+++ src/czi/ai/rbio/utils/utils.py
@@ -1,19 +1,117 @@
+import hashlib
+import json
 import re
 
+import numpy as np
 
-def extract_answer(text, binary = False):
-    if binary:
-        found = re.search(r"<answer>\s*(yes|no)\s*</answer>", text, re.IGNORECASE)
-        if found:
-            if found.group(1).strip().lower() == "yes":
-                return True
-            if found.group(1).strip().lower() == "no":
-                return False
-    else:
-        found = re.search(r"<answer>(.*?)</answer>", text, re.IGNORECASE)
-        if found:
-            return found.group(1).strip().lower()
-    return None
+# General system prompt
+SYSTEM_PROMPT = "A conversation between User and Biologist. The user asks a question, and the Biologist solves it. The biologist first thinks about the reasoning process in the mind and then provides the user with the answer. The reasoning process and answer are enclosed within <think> </think> and <answer> </answer> tags, respectively, i.e., <think> reasoning process here </think> <answer> answer here </answer>."
+
+
+def normalize_scores(m, sig_threshold, reverse=False):
+    """
+    Normalizes a matrix m to the interval [0, 1] such that values at a given signifcance threshold
+    sig_threshold correspond to values at 0.5 in the unit-normalized matrix m_norm
+
+    No assumption are made on the range of values in m or on sig_threshold
+
+    Args:
+        m: matrix to normalize
+        sig_threshold: significance threshold in
+    """
+    m_norm = np.zeros(m.shape)
+    m_min = m.min()
+    m_max = m.max()
+    m_below_mask = m < sig_threshold
+    m_above_mask = m >= sig_threshold
+    m_below_norm = 0.5 * (m[m_below_mask] - m_min) / (sig_threshold - m_min)
+    m_above_norm = 0.5 + 0.5 * (m[m_above_mask] - sig_threshold) / (
+        m_max - sig_threshold
+    )
+
+    # Adjust for when values lower than sig_threshold should map to [0.5, 1],
+    # rather than values > sig_threshold
+    if reverse:
+        m_above_norm -= 0.5
+        m_below_norm += 0.5
+
+    m_norm[m_below_mask] = m_below_norm
+    m_norm[m_above_mask] = m_above_norm
+    print(
+        f"Matrix normalized. Values < {sig_threshold:0.4f} mapped to [{m_norm[m_below_mask].min():0.2f}, {m_norm[m_below_mask].max():0.2f}]. Values >= {sig_threshold:0.4f} mapped to [{m_norm[m_above_mask].min():0.2f}, {m_norm[m_above_mask].max():0.2f}] | m_norm_min: {m_norm.min():0.4f}. m_norm_max: {m_norm.max():0.4f}"
+    )
+    return m_norm
+
+
+def compute_binary_class_confidences(x):
+    """
+    Computes binary class confidences corresponding to the CSV schema for soft verification
+    datasets, given a series x.
+
+    x is assumed to have the following fields:
+     - classes: all class labels
+     - label: the ground truth class label
+
+    The function returns a string correponding to class_confidences for all the classes
+
+    Example:
+        classes = yes|no
+        label = yes
+
+    Function returns class_confidences = 1|0
+
+    Args:
+        x: dataframe
+    """
+    classes = x["classes"].split("|")
+    gt_class = x["label"]
+    class_confidences = []
+    for cl in classes:
+        class_confidences.append(str(int(cl == gt_class)))
+    return "|".join(class_confidences)
+
+
+def compute_soft_class_confidences(x, gene_pair2scores, fields):
+    """
+    Computes soft class confidences corresponding to the CSV schema for soft verification
+    datasets, given a series x.
+
+    x is assumed to have the following fields:
+     - classes: all class labels
+     - label: the ground truth class label
+     - fields: list of two fields corresponding to a pair of genes
+
+    The function returns a string correponding to class_confidences for all the classes for the pair of genes
+
+    Example:
+        fields[0]: gene_A
+        fields[1]: gene_B
+        classes = yes|no
+        label = yes
+
+    Function returns class_confidences = gene_pair2scores[(gene_A, gene_B)]|0
+
+    Args:
+        x: dataframe
+        gene_pair2scores: dictionary corresponding to gene pairs and confidence interaction scores
+    """
+    gene_A = x[fields[0]]
+    gene_B = x[fields[1]]
+    pmi = gene_pair2scores[(gene_A, gene_B)]
+    classes = x["classes"].split("|")
+    gt_class = x["label"]
+    class_confidences = []
+    for cl in classes:
+        class_confidences.append(str(int(cl == gt_class) * pmi))
+    return "|".join(class_confidences)
+
+
+def extract_binary_answer(text):
+    found = re.search(r"<answer>\s*(yes|no)\s*</answer>", text, re.IGNORECASE)
+    if found:
+        return found.group(1).strip().lower()
+
+    return ""
 
 
 def extract_think(text, separator="\n"):
@@ -24,10 +122,17 @@ def extract_think(text, separator="\n"):
 
 
 def extract_gene_info(text, gene):
-    # print(text)
-    found = re.search(f'<gene_info>\s*(.*?{gene}.*?)\s*</gene_info>', text, re.IGNORECASE)
+    found = re.search(
+        f"<gene_info>\s*(.*?{gene}.*?)\s*</gene_info>", text, re.IGNORECASE
+    )
     if found:
         matches = found.group(1).strip()
         if gene in matches:
             return matches
     return f"No information."
+
+
+def compute_embeddings_hash(emb_dict: dict) -> str:
+    # Convert embeddings to a stable string representation
+    emb_str = json.dumps({k: v.tolist() for k, v in sorted(emb_dict.items())})
+    return hashlib.md5(emb_str.encode()).hexdigest()
