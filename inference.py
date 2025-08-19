1b4e967d6d34ec8bc773a48e3a34297b10c8c954
Omar Valenzuela <omar.valenzu7@gmail.com>
2025-08-19 11:24:51 -0700

    fix typo in rbio_model_ckpt help flag

--- inference.py
+++ inference.py
@@ -240,7 +240,7 @@ def inference_fn(
 @click.option(
     "--base_model_name", help="base model name", default="Qwen/Qwen2.5-3B-Instruct"
 )
-@click.option("--rbio_model_ckpt", help="rbio1-3b", default="rbio1-TF")
+@click.option("--rbio_model_ckpt", help="rbio1-TF", default="rbio1-TF")
 @click.option(
     "--results_output_folder",
     help="optional folder where to save the results",
